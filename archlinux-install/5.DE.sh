b11cb6aa03d674fa91b8a03535a49088060f4391
22earth <zhifengle@gmail.com>
2019-06-22 13:34:44 +0800

    update install DE script

--- archlinux-install/5.DE.sh
+++ archlinux-install/5.DE.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 
 de="xorg i3 xfce4 lightdm lightdm-gtk-greeter xorg-server"
-softwares="rofi tmux xsel ttf-dejavu"
+softwares="rofi tmux xsel ttf-dejavu wqy-zenhei wqy-microhei"
+
+## autologin
+
+## archlinuxcn
+# /etc/pacman.conf
+#[archlinuxcn]
+##The Chinese Arch Linux communities packages.
+#SigLevel = Optional TrustedOnly
+#Server   = https://mirrors.ustc.edu.cn/archlinuxcn/$arch
+pacman -S archlinuxcn-keyring
+
+
+## vmware-tools
+#pacman -S open-vm-tools
+
+# 让tty支持中文 是否过时？
+#sudo pacman -S fbterm
+#sudo gpasswd -a YOUR_USERNAME video
+
 
 
