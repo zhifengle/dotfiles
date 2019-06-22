f8b0efdaf82ebd01b97e4421957bbe95f6e7ab12
22earth <zhifengle@gmail.com>
2019-06-22 11:41:03 +0800

    add archlinxu install script

---
+++ archlinux/2.env-config.sh
@@ -0,0 +1,14 @@
+#pacman -S git vim
+ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+hwclock --systohc
+echo -e "en_US.UTF-8 UTF-8\nzh_CN.UTF-8 UTF-8" >> /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
+echo "myarch" > /etc/hostname
+host='
+127.0.0.1	localhost
+::1		localhost
+127.0.1.1	myarch.localdomain	myarch
+'
+echo "$host" > /etc/hosts
+
